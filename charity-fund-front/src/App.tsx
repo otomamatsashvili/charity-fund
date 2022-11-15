5277ced442dc2e82c02e0c13b0c62b6da293b072
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-11-15 11:17:47 +0400

    chore: add front folder

---
+++ charity-fund-front/src/App.tsx
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <div>
+      <Button>Hello there</Button>
+    </div>
+  );
+};
+
+export default App;
