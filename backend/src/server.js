79660efccb2a77d498019d8a11bb1c213b702264
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-11-15 12:53:54 +0400

    feat: add mongo database connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/db.js';
 
 const server = express();
+
+connectDB();
 
 server.use(cors());
 
